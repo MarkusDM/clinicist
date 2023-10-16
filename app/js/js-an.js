3f4496814904e0a13a6c92f5c3b9a40f65de5832
AstasiaG <anstasa.li@gmail.com>
2023-10-16 21:39:36 +0400

    feat: add about page desk

--- app/js/js-an.js
+++ app/js/js-an.js
@@ -1,3 +1,14 @@
+const storyItems = Array.from(document.querySelectorAll('.story__item'));
+const storyBottoms = Array.from(document.querySelectorAll('.story__bottom'));
+storyItems.forEach( e => {
+  e.addEventListener('mouseenter', () => {
+    e.childNodes[7].classList.add('active');
+  })
+  e.addEventListener('mouseleave', () => {
+    e.childNodes[7].classList.remove('active');
+  })
+})
+
 const proceduresSwiper = new Swiper('.procedures__item-swiper', {
   navigation: {
     nextEl: '.procedures__item-btn-next',
@@ -8,27 +19,58 @@ const proceduresSwiper = new Swiper('.procedures__item-swiper', {
   speed: 1000,
 })
 
+const valueThumbs = new Swiper('.thumbs-swiper', {
+  slideToClickedSlide: true,
+  slidesPerView: 'auto',
+  touchRatio: 0.2,
+})
+
 const valueslider = new Swiper('.values-swiper', {
-  //direction: 'vertical',
-  wrapperClass: '.values__wrapper',
-  slideClass: '.values__right-slide',
-  navigation: {
-    nextEl: '.values-btn-next',
-    prevEl: '.values-btn-prev',
-  },
   slidesPerView: 1,
   spaceBetween: rem(1),
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  // thumbs: {
+  //   swiper: valueThumbs
+  // },
   speed: 1000,
 })
 
+valueslider.controller.control = valueThumbs;
+valueThumbs.controller.control = valueslider;
 
 
-const quoteSlider = new Swiper('.quote-swiper', {
+const quoteSlider = new Swiper('.licenzia-swiper', {
+  //loop: true,
   navigation: {
-    nextEl: '.quote-btn-next',
-    prevEl: '.quote-btn-prev',
+    nextEl: '.licenzia-btn-next',
+    prevEl: '.licenzia-btn-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
   },
   slidesPerView: 4,
-  spaceBetween: rem(0.5),
+  spaceBetween: rem(2.1),
+  speed: 1000,
+})
+
+const aboutSlider = new Swiper('.about-swiper', {
+  navigation: {
+    nextEl: '.about-btn-next',
+    prevEl: '.about-btn-prev',
+  },
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+  },
+  slidesPerView: 1,
+  spaceBetween: rem(1),
   speed: 1000,
 })
