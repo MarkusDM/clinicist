98d51670b71abcc2a638c206f71939ab00d7bbf9
Timofey Agapitov <timonchikagapitov@gmail.com>
2023-10-22 16:19:19 +0300

    fix

--- build/js/main-t.js
+++ build/js/main-t.js
@@ -1,4 +1,41 @@
+document.addEventListener('DOMContentLoaded', function () {
+  $(document).ready(function ($) {
+    if (document.querySelector('#accordion-questions')) {
+      $('#accordion-questions').accordionjs();
+    }
+  });
 
-$(document).ready(function ($) {
-  $('#accordion-questions').accordionjs();
+  if (document.querySelector('.diseases-doctors__slide')) {
+    if (md) {
+      ourDoctorsSlider = new Swiper('.diseases-doctors__slider', {
+        spaceBetween: 40,
+        slidesPerView: 1,
+        speed: 1000,
+        autoHeight: true,
+        navigation: {
+          nextEl: '.diseases-doctors__slider-control .btn-next',
+          prevEl: '.diseases-doctors__slider-control .btn-prev',
+        },
+        pagination: {
+          el: '.diseases-doctors__slider-pagination',
+          clickable: true,
+        },
+      });
+    } else if (!md) {
+      ourDoctorsSlider = new Swiper('.diseases-doctors__slider', {
+        spaceBetween: 40,
+        slidesPerView: 2,
+        observer: true,
+        observeParents: true,
+        speed: 1000,
+        autoHeight: false,
+        observer: true,
+        observeParents: true,
+        navigation: {
+          nextEl: '.diseases-doctors__slider-control .btn-next',
+          prevEl: '.diseases-doctors__slider-control .btn-prev',
+        },
+      });
+    }
+  }
 });
