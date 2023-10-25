0993388320ae96180c901de4ca1ddbbb8d85ef60
DiMoNiO04 <dima.razumov.940@mail.ru>
2023-10-25 12:27:27 +0300

    refactor: page blog-article

--- build/js/js-dim.js
+++ build/js/js-dim.js
@@ -4,6 +4,8 @@ $( document ).ready(function() {
 
     $('.input_email input').inputmask('email');
 
+    $('.tel-mask-2').inputmask('+7 (999) 999-99-99');
+
     $('.js-toggle').on('click', function () {
       $('.js-toggle').removeClass('active')
       $(this).toggleClass('active')
@@ -16,37 +18,41 @@ $( document ).ready(function() {
       })
     })
 
-    const newsArticleSlidder = new Swiper('.news-descs__desc-slider', {
-      spaceBetween: rem(1),
-      loop: false,
-      autoplay: {
-        delay: 4000,
-      },
-      speed: 1000,
-      navigation: {
-        nextEl: '.news-descs__desc-slider .btn-next',
-        prevEl: '.news-descs__desc-slider .btn-prev',
-      },
-      pagination: {
-        el: '.news-descs__desc-slider .swiper-pagination',
-        clickable: true,
-      },
-    })
+    if($('.news-descs__desc-slider')) {
+      const newsArticleSlidder = new Swiper('.news-descs__desc-slider', {
+        spaceBetween: 10,
+        loop: false,
+        autoplay: {
+          delay: 4000,
+        },
+        speed: 1000,
+        navigation: {
+          nextEl: '.news-descs__desc-slider .btn-next',
+          prevEl: '.news-descs__desc-slider .btn-prev',
+        },
+        pagination: {
+          el: '.news-descs__desc-slider .swiper-pagination',
+          clickable: true,
+        },
+      })
+    }
 
-    const blogArticleSlidder = new Swiper('.blog-desc__desc-slider', {
-      spaceBetween: rem(1),
-      loop: false,
-      autoplay: {
-        delay: 4000,
-      },
-      speed: 1000,
-      navigation: {
-        nextEl: '.blog-desc__desc-slider .btn-next',
-        prevEl: '.blog-desc__desc-slider .btn-prev',
-      },
-      pagination: {
-        el: '.blog-desc__desc-slider .swiper-pagination',
-        clickable: true,
-      },
-    })
+    if($('.blog-desc__desc-slider')) {
+        const blogArticleSlidder = new Swiper('.blog-desc__desc-slider', {
+        spaceBetween: 10,
+        loop: false,
+        autoplay: {
+          delay: 4000,
+        },
+        speed: 1000,
+        navigation: {
+          nextEl: '.blog-desc__desc-slider .btn-next',
+          prevEl: '.blog-desc__desc-slider .btn-prev',
+        },
+        pagination: {
+          el: '.blog-desc__desc-slider .swiper-pagination',
+          clickable: true,
+        },
+      })
+    }
 })
